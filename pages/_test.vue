ddacf84b94e39c60aa65a21b76926e893882fc82
Coffee-crocodile <1147347984@qq.com>
2023-04-26 17:42:44 +0800

    test(test): modify show var

--- pages/_test.vue
+++ pages/_test.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 const config = useRuntimeConfig()
-const envData = process.env
 </script>
 
 <template>
@@ -8,9 +7,5 @@ const envData = process.env
     <div>=== RuntimeConfig.public ===</div>
     <div>{{ config.public.apiBase }}</div>
     <div>{{ config.public.sourcesBase }}</div>
-    <div>=== process.env ===</div>
-    <div v-for="(value, key, index) in envData" :key="index">
-      {{ key }} : {{ value }}
-    </div>
   </div>
 </template>
